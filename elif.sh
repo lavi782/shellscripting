5fff024d7d23bbc1a31a4ffd68b8b44c458bc6e9
lavi782 <lavanyasake782@gmail.com>
2024-12-02 15:00:30 +0530

    added elif script

---
+++ elif.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+echo "enter a value: "
+read a
+echo "enter b value : "
+read b
+if [ $a == $b ]
+then
+        echo "a is equal to b"
+elif
+        echo " a is not equal to b"
+then 
+
+else 
+
+   echo " give correct inputs"
+
+fi   
