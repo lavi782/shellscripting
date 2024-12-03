b4cc807a9056cfe304aeec8b427df3b26561bf78
lavi782 <lavanyasake782@gmail.com>
2024-12-03 11:41:16 +0530

    use arthametic operators

---
+++ arthametics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "enter 1st number"
+read num1
+echo "enter 2nd number"
+read num2
+echo "additions":$((num1 + num2))
+echo "substactions":$((num1 - num2))
+echo "multiplications":$((num1 * num2))
+
