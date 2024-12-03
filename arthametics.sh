7dfebc77b0b4a1edc993f4b2b36ecc446ea3702a
lavi782 <lavanyasake782@gmail.com>
2024-12-03 12:05:56 +0530

    use arthametic operators

--- arthametics.sh
+++ arthametics.sh
@@ -3,7 +3,6 @@ echo "enter 1st number"
 read num1
 echo "enter 2nd number"
 read num2
-echo "additions":$((num1 + num2))
-echo "substactions":$((num1 - num2))
-echo "multiplications":$((num1 * num2))
+echo "scale=2:$num1/$num2" | bc
+
 
