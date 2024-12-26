eb42010bd8f0ea7dbf5dd076a6d8db9cdaf9a3c0
lavi782 <lavanyasake782@gmail.com>
2024-12-26 11:35:08 +0530

    combine operator

--- arthametics.sh
+++ arthametics.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-read var_name
-echo "you entered: $var_name"
-
+a=10
+b=5
+c=15
+if [ $a -gt 5] && [&b -gt 10] || [$c -gt 15];then
+echo "condition met"
+else 
+echo "condition not met"
+fi
