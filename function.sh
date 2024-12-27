c1e96941c45729ec841f65dd3551972edcc0e08b
lavi782 <lavanyasake782@gmail.com>
2024-12-27 11:59:00 +0530

    shift loop

--- function.sh
+++ function.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-check_even(){
-    if (( $1 % 2 == 0 )); then
-echo "$1 is even"
-else
-echo "$1 is odd"
-fi
-}
-check_even 8   
+# check_even(){
+#     if (( $1 % 2 == 0 )); then
+# echo "$1 is even"
+# else
+# echo "$1 is odd"
+# fi
+# }
+# check_even 8   
+
+
+process_first_two(){
+    echo "first argument : $1"
+    echo "second argument : $2"
+    shift 2
+    echo "remaining argument : $@"
+    }
+    process_first_two "apple" "banana" "orange" "churry"
